16cb6e2cc5babde2f2a41118ef981913d5bca9e9
kataya1 <mohamedkataya1@gmail.com>
2022-01-30 20:06:23 +0200

    typescript documentation day

--- ch1/7/7.ts
+++ ch1/7/7.ts
@@ -1,5 +1,7 @@
 // N x N matrix, pixes have 4 bytes ( 32 bits )
 // how to tell typescript that it's 2 dimenional array and the element are 4 bytes 
+// var arr_name:datatype[][]=[ [val1,val2,val3],[v1,v2,v3] ]
+
 // need to execlued non primary degrees
 type matrix = [][][] //?
 // const rotate =  ( m: matrix, deg)
