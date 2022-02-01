9213439e9b8d3207ecb2167cdc216078d44654ca
kataya1 <mohamedkataya1@gmail.com>
2022-02-01 19:25:43 +0200

    1.7 complete

--- ch1/7/7.ts
+++ ch1/7/7.ts
@@ -6,11 +6,19 @@ let img: matrix = [ [ 0xff003322, 0xe5e5e5e5, 0xe3e3e3e3, 0x11111111, 0xffffffff
             [ 0xff003322, 0xe5e5e5e5, 0xe3e3e3e3, 0x11111111, 0xffffffff ],
             [ 0xff003322, 0xe5e5e5e5, 0xe3e3e3e3, 0x11111111, 0xffffffff ]
         ]
-let img2 = [[1, 2, 3, 4, 5],
+let img2: matrix = [[1, 2, 3, 4, 5],
         [6, 7, 8, 9, 10],
         [11, 12, 13, 14, 15],
         [16, 17, 18, 19, 20],
         [21, 22, 23, 24, 25]
+    ];
+let img22: matrix = [
+    [10, 11, 12, 13, 14, 15],
+    [20, 21, 22, 23, 24, 25],
+    [30, 31, 32, 33, 34, 35],
+    [40, 41, 42, 43, 44, 45],
+    [50, 51, 52, 53, 54, 55],
+    [60, 61, 62, 63, 64, 65],
     ];
 let img3 = [[1, 2, 3, 4, 5],
             [6, 7, 8, 9, 10]
@@ -32,9 +40,26 @@ const rotate =  ( m: matrix, deg: 'left' | 'right' = 'right'): matrix =>{
     return newImg
 }
 
+const rotateInPlace = (m: matrix): matrix =>{
+    let n = m.length
+    let temp: number;
+    for (let i = 0; i<= n / 2; i++){
+        for( let j = i; j< n - 1 - i; j++ ){
+            temp = m[i][j]
+            m[i][j]  = m[ n - 1 - j][i]
+            m[ n - 1 - j][i] = m[n - 1 - i][n - 1 - j]
+            m[n - 1 - i][n - 1 - j] = m[ j ] [ n - 1 - i]
+            m[ j ] [ n - 1 - i] = temp
+        }
+    }
+    return m
+}
+
 
 // console.log(rotate(img))
 // console.log(rotate(img2))
 //this soloution can rotate N x M arrays left or right, O(NxM) space and time
-console.log(rotate(img3))
-console.log(rotate(img3, 'left'))
+// console.log(rotate(img3))
+// console.log(rotate(img3, 'left'))
+console.log(rotateInPlace(img2))
+console.log(rotateInPlace(img22))
