bdaef33ca0f1a9fd901d821903dc1c6df18a6934
kataya1 <mohamedkataya1@gmail.com>
2022-03-19 18:10:12 -0400

    2.6 LL palindrome O(n) space

--- ch2/2.6_Palindrome.ts
+++ ch2/2.6_Palindrome.ts
@@ -16,6 +16,7 @@ pali.enqueu('a').enqueu('b').enqueu('b').enqueu('a')
 console.log(notPali.printChain())
 
 function isPalindrome(n: LLNode<any> | null): boolean{
+    // O(n) time ,  O(n) space
     let p: typeof n = n
     let s = new LinkedList()
     let c = 0
@@ -36,3 +37,32 @@ function isPalindrome(n: LLNode<any> | null): boolean{
 console.log(isPalindrome(ll.head))  // true
 console.log(isPalindrome(notPali.head))  // false
 console.log(isPalindrome(pali.head))  // true
+
+function reversLL(node: LLNode< any>){
+    let prev = null
+    let temp;
+    while(node != null){
+        temp = node.next
+        node.next = prev
+        prev = node 
+        node = temp
+    }
+    return prev
+}
+
+function isPalindrome2(n){
+    // o(n) time  , O(1) space
+    let slow = n, fast = n
+    while( fast && fast.next){
+        slow = slow.next
+        fast = fast.next.next
+    }
+    let end = reversLL(slow)
+    
+    while( end && n){
+        if( end.data !== n.data) return false
+        end = end.next
+        n = n.next
+    }
+    return true 
+}
