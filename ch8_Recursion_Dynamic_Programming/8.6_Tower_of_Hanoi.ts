682167ebe94d610daf70a84eef704ca354f5ac38
kataya1 <mohamedkataya1@gmail.com>
2022-04-06 17:19:32 -0400

    Tower of Hanoi

---
+++ ch8_Recursion_Dynamic_Programming/8.6_Tower_of_Hanoi.ts
@@ -0,0 +1,76 @@
+class Disc extends Number{
+    value: number;
+    constructor(v: number){
+        super(v)
+        this.value = v
+    }
+}
+
+class Tower{
+    stack: Array<number> = []
+    name: string;
+    constructor(name: string){
+        this.name = name
+    }
+    add(d: number) {
+        if( this.size() === 0 || (this.peek() && this.peek()! > d))
+            this.stack.push(d) 
+        else
+            throw new Error('what the fuck')
+    }
+    moveTopTo(t: Tower): boolean {
+        if (!this.size()) return false
+        let p = this.stack.pop()
+        t.add(p!)
+        return true
+    }
+    peek(): number | undefined{
+        if( this.size() == 0)
+            return 
+        return this.stack[this.size() - 1 ]
+    }
+    size(): number{
+        return this.stack.length
+    }
+}
+class Hanoi{
+    origin: Tower
+    buffer: Tower
+    destination: Tower
+    numOfDiscs: number
+    steps: string[] = []
+    constructor(numOfDiscs: number){
+        this.origin = new Tower('src ')
+        this.buffer = new Tower('buff')
+        this.destination = new Tower('dest')
+        for( let i = numOfDiscs ; i > 0 ; i--){
+            this.origin.add( i )
+        }
+        this.numOfDiscs = numOfDiscs
+    }
+    print(){
+        console.log('origin: ', this.origin.stack)
+        console.log('buffer: ', this.buffer.stack)
+        console.log('destination: ', this.destination.stack)
+        console.log('\n')
+    }
+
+    solve(origin = this.origin, destination = this.destination, buffer = this.buffer, n = this.numOfDiscs){    
+        if ( n == 0) return 
+        if( n == this.numOfDiscs) this.print()
+        this.solve(  origin , buffer, destination, n - 1)
+        this.steps.push(`move ${origin.peek()} from ${origin.name} to ${destination.name}\n`)
+        origin.moveTopTo(destination)
+        this.print()
+        this.solve( buffer, destination, origin , n - 1)
+    }
+    printSteps(){
+        console.log(this.steps.toString())
+    }
+
+}
+
+let h = new Hanoi(4)
+
+h.solve()
+h.printSteps()
