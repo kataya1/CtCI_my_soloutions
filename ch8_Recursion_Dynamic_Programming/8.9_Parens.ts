69d6560e52b25c8e61baabfb29afec618b6e0d98
kataya1 <mohamedkataya1@gmail.com>
2022-04-10 15:31:37 -0400

    8.9 parens

--- ch8_Recursion_Dynamic_Programming/8.9_Parens.ts
+++ ch8_Recursion_Dynamic_Programming/8.9_Parens.ts
@@ -1,10 +1,36 @@
-function parens(n: number, arr =  [new Set("()") ]): string[]{
-    if (n < 1 ) return [""]
-    if (!arr[n])
-        arr[n] = new Set([...parens(n-1, arr).map(v=>  `(${v})`), ...parens(n-1, arr).map(v =>  `()${v}`), ...parens(n-1, arr).map(v=>  `${v}()`)] )
-    return [...arr[n]]
+function parens(n: number, arr = [new Set("()")]): string[] {
+  if (n < 1) return [""];
+  if (!arr[n])
+    arr[n] = new Set([
+      ...parens(n - 1, arr).map((v) => `(${v})`),
+      ...parens(n - 1, arr).map((v) => `()${v}`),
+      ...parens(n - 1, arr).map((v) => `${v}()`),
+    ]);
+  return [...arr[n]];
 }
 
-console.log( parens(2))
-console.log( parens(3))
-console.log( parens(4))
+console.log(parens(2));
+console.log(parens(3));
+console.log(parens(4));
+
+function getParen(list: string[], leftRem: number , rightRem: number, str: string[] = [] , index = 0){
+    if ( leftRem < 0 || rightRem < leftRem)
+     return 
+    if (leftRem == 0 && rightRem ==0)
+        list.push( str.join(''))
+    else{
+        str[index] = '('
+        getParen( list, leftRem - 1, rightRem, str, index + 1)
+
+        str[index] = ')'
+        getParen( list, leftRem, rightRem - 1, str, index + 1)
+    }
+}
+function parenOptimize(n: number): string[]{
+    let  validPerms: string[] = []
+    getParen(validPerms, n, n )
+    return validPerms
+}
+
+console.log(parenOptimize(3))
+console.log(parenOptimize(4))
