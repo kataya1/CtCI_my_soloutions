a974b0a4719c9a830b250344fd2518b4c38cd2c7
kataya1 <mohamedkataya1@gmail.com>
2022-04-17 18:56:38 -0400

    backtracking thoughts

--- ch8_Recursion_Dynamic_Programming/8.11_Coins.ts
+++ ch8_Recursion_Dynamic_Programming/8.11_Coins.ts
@@ -4,8 +4,10 @@ function coinCombinationForNCent( n: number, ways: number[][] = [], quarters = 0
     // i only need to calculate the number not the ways 
     // this will but a stain on memory 
 
-    if ( n < 0 || n % 1 != 0) return
+    if ( n < 0 || n % 1 != 0) return  // if the coins left is less than 0 or if n is not an integere ( we don't have half pennies )
     else if ( n == 0) ways.push( [ quarters, dimes,  nickles, pennies ])
+    // instead of passing prematives here  which would cause O(4n)space 
+    // we could pass an array and backtrack like arr[index] -= 1   will be O( 4) space
     else{
         if( pennies == 0 && (dimes == 0) && nickles ==  0)
             coinCombinationForNCent( n - 25, ways, quarters + 1, dimes, nickles, pennies )
