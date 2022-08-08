60189907bfe60502f7fce4e040ab2d40525cd9f0
kataya1 <mohamedkataya1@gmail.com>
2022-08-07 23:16:57 -0400

    1.1

--- ch1_Array/1/1.ts
+++ ch1_Array/1/1.ts
@@ -1,22 +1,45 @@
-export const hasUniqueChars = ( s: string): boolean => {
+const hasUniqueChars3 = (s: string): boolean => {
+	for (let i = 0; i < s.length; i++) {
+		for (let j = i + 1; j < s.length; j++) {
+			if (s[i] == s[j]) return false
+		}
+	}
+	return true
+}
+
+
+
+
+
+
+
+
+
+
+console.log(hasUniqueChars3("abc")) //true
+console.log(hasUniqueChars3("abcc")) //false
+console.log(hasUniqueChars3("")) //true
+
+export const hasUniqueChars = (s: string): boolean => {
 	// O(n)
 	let uniqueVals = new Set()
-    for ( let i of s){
-		if ( uniqueVals.has(i)) return false
+	for (let i of s) {
+		if (uniqueVals.has(i)) return false
 		uniqueVals.add(i)
 	}
-	 
+
 	return true
 }
 
 export const hasUniqueChars2 = (s: string): boolean => {
 	// O( nlogn)
 	let sortedS = [...s].sort()
-	for ( let i = 0 ; i < sortedS.length - 1; i++){
-		if ( sortedS[i] === sortedS[i + 1]) return false 
+	for (let i = 0; i < sortedS.length - 1; i++) {
+		if (sortedS[i] === sortedS[i + 1]) return false
 	}
 	return true
 }
+const hasUniqueChars4 = (s: string): boolean => new Set(s).size === s.length
 
 let a = "n39ri1yaim"
 let b = "abcdefghijklmmnopqrstuvwxyz"
@@ -24,5 +47,5 @@ console.log(hasUniqueChars(a))
 console.log(hasUniqueChars(b))
 console.log(hasUniqueChars2(a))
 console.log(hasUniqueChars2(b))
-// test 
-module.exports = hasUniqueChars
+// test
+// module.exports = hasUniqueChars
