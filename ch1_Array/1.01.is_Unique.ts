51872fff3b83954cac596e206736ab291c3f54d0
kataya1 <mohamedkataya1@gmail.com>
2023-03-01 15:49:17 -0500

    bit vector implementation

--- ch1_Array/1.01.is_Unique.ts
+++ ch1_Array/1.01.is_Unique.ts
@@ -1,16 +1,3 @@
-const hasUniqueChars3 = (s: string): boolean => {
-    for (let i = 0; i < s.length; i++) {
-        for (let j = i + 1; j < s.length; j++) {
-            if (s[i] == s[j]) return false
-        }
-    }
-    return true
-}
-
-
-console.log(hasUniqueChars3("abc")) //true
-console.log(hasUniqueChars3("abcc")) //false
-console.log(hasUniqueChars3("")) //true
 
 export const hasUniqueChars = (s: string): boolean => {
     // O(n)
@@ -32,11 +19,53 @@ export const hasUniqueChars2 = (s: string): boolean => {
     return true
 }
 
-let a = "n39ri1yaim"
-let b = "abcdefghijklmmnopqrstuvwxyz"
-console.log(hasUniqueChars(a))
-console.log(hasUniqueChars(b))
-console.log(hasUniqueChars2(a))
-console.log(hasUniqueChars2(b))
+// let a = "abc"
+// let b = "abcda"
+// console.log(hasUniqueChars(a))  //true
+// console.log(hasUniqueChars(b))  //false
+// console.log(hasUniqueChars2(a)) //true
+// console.log(hasUniqueChars2(b)) //false
 // test
 // module.exports = hasUniqueChars
+
+const hasUniqueChars3 = (s: string): boolean => {
+    for (let i = 0; i < s.length; i++) {
+        for (let j = i + 1; j < s.length; j++) {
+            if (s[i] == s[j]) return false
+        }
+    }
+    return true
+}
+
+// ====== 
+
+// JavaScript Uses 32 bits Bitwise Operands
+// 00000000000000000000000000000101(5)
+// 11111111111111111111111111111010(-5 = -6)
+
+console.log(1 << 0) // 1
+console.log(1 << 1) // 2
+console.log(1 << 30) // 1073741824
+console.log(1 << 31) // -2147483648
+console.log(1 << 32) // 1   mening 0 and 32 is equivelant
+console.log(1 << 33) // 2
+const hasUniqueChars4 = (s: string): boolean => {
+    // Won't work because JavaScript Uses 32 bits Bitwise Operands
+    let bitVector = 0
+    for (let c of s) {
+        bitVector
+        if ((1 << c.charCodeAt(0) & bitVector) > 0)
+            return false
+
+        bitVector = bitVector | (1 << c.charCodeAt(0))
+    }
+    return true
+}
+
+// console.log(hasUniqueChars3("abc")) //true
+// console.log(hasUniqueChars3("abcc")) //false
+// console.log(hasUniqueChars3("")) //true
+
+
+
+
